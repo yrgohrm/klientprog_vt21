97908436e16ee4c7fd693f093ee6f41a2f9ad350
Hampus Ram <hampus.ram@educ.goteborg.se>
2021-01-28 10:57:46 +0100

    uppdateringar lektion 6

---
+++ lektion6/js/addp.js
@@ -0,0 +1,7 @@
+
+for (let i = 0; i < 5; ++i) {
+    let myPElem = document.createElement('p')
+    myPElem.innerText = "hejsan världen"
+
+    document.body.appendChild(myPElem)
+}
---
+++ lektion6/js/script.js
@@ -0,0 +1,2 @@
+console.log("Hejsan världen")
+
