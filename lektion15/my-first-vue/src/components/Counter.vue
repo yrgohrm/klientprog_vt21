b6851ea02a1c1d2337cc53293b9b7212520eb03f
Hampus Ram <hampus.ram@educ.goteborg.se>
2021-03-16 14:00:02 +0100

    lektion 15

---
+++ lektion15/my-first-vue/src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button @click="incrementCounter">Öka!</button>
+        <p>{{ count }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    emits: ["count-incremented"],
+    data() {
+        return {
+            count: 0,
+        }
+    },
+    methods: {
+        incrementCounter() {
+            this.count++
+            this.$emit("count-incremented", this.count)
+        },
+    },
+}
+</script>
---
+++ lektion15/my-first-vue/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <h2>{{ myCount }}</h2>
+        <counter @count-incremented="doStuffWhenCountIsIncremented" />
+    </div>
+</template>
+
+<script>
+import Counter from "./Counter.vue"
+
+export default {
+    name: "HelloWorld",
+    components: {
+        Counter,
+    },
+    props: {
+        msg: String,
+    },
+    data() {
+        return {
+            myCount: 0,
+        }
+    },
+    methods: {
+        doStuffWhenCountIsIncremented(evt) {
+            this.myCount = evt
+        },
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+</style>
